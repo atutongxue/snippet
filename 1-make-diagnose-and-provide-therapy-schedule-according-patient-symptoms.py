6bcee2b067560bce6fef135937c721929c407cba
阿图同学 <76881401+atutongxue@users.noreply.github.com>
2021-01-13 20:01:08 +0800

    Update and rename 1-make-diagnose-and-provide-therapy-schedule-accoring-patient-symptoms.py to 1-make-diagnose-and-provide-therapy-schedule-according-patient-symptoms.py

--- 1-make-diagnose-and-provide-therapy-schedule-according-patient-symptoms.py
+++ 1-make-diagnose-and-provide-therapy-schedule-according-patient-symptoms.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # 功能：根据患者症状，做出诊断，并统计治疗方案
 # 示例：如果只有发热、寒颤，就诊断为感冒；如果只有鼻塞、咳嗽，就诊断为肺炎; 全有的话，可能性各50%
-# 来源：https://github.com/atutongxue/snippet/blob/main/1-make-diagnose-and-provide-therapy-schedule-accoring-patient-symptoms.py
+# 来源：https://github.com/atutongxue/snippet/blob/main/1-make-diagnose-and-provide-therapy-schedule-according-patient-symptoms.py
 
 # 定义症状
 symptoms = ['fever', 'chill', 'stuffy', 'cough']
@@ -27,7 +27,9 @@ therapy = {
 }
 
 # 输入病人症状
-string_symptom = str(input('请输入病人症状字符串，长度4位，每位代表是否发烧、寒颤、鼻塞、咳嗽，例如4项症状都有对应的字符串为1111，有发热、有寒颤、无鼻塞、无咳嗽的字符串为1100，依此类推：'))
+print('为方便输入，病人症状由4位二进制字符串表示，每位代表是否有发热、寒颤、鼻塞、咳嗽症状，有症状为1，无症状为0。')
+print('例如有发热、有寒颤、无鼻塞、无咳嗽，输入1100，4项症状都有，输入1111，依此类推。')
+string_symptom = str(input('在这里输入4位二进制字符串：'))
 
 # 得到病人症状列表
 patient_symptoms = [symptoms[i] for i, bit in enumerate(string_symptom) if bit == '1']
@@ -37,21 +39,19 @@ result = [diag_name for diag_name, diag_symptoms in  diagnoses.items() if set(di
 
 # 输出诊断结果并给出诊断方案。如果只有发热、寒颤，就诊断为感冒；如果只有鼻塞、咳嗽，就诊断为肺炎；如果4项症状全有，可能性各占50%
 if len(result) == 1:
-    print('诊断结果为：'+result[0]+'，可能性90%，这是各位医生给出的治疗方案：')
+    print('\n诊断结果为：'+result[0]+'，可能性90%，这是各位医生给出的治疗方案：')
     print(therapy[result[0]])
 
-else if len(result) == 2:
-    print('诊断结果为: '+result[0]+'可能性50%，'+result[1]+'可能性50%。')
+elif len(result) == 2:
+    print('\n诊断结果为: '+result[0]+'可能性50%，'+result[1]+'可能性50%。')
     print('对于'+result[0]+'，这是各位医生给出的治疗方案：')
     print(therapy[result[0]])
     print('对于'+result[1]+'，这是各位医生给出的治疗方案：')
     print(therapy[result[1]])
 else:
-    print('没有找到对应的诊断结果，请丰富数据库')
+    print('\n没有找到对应的诊断结果，请丰富数据库')
 
 #  do some analyze here 此处做一些分析
 #  get some result here 此处生成一些结果
 
-
-
-
+input("\n程序运行完毕，按[回车键]结束...")
